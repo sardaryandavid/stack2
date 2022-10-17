3efc4336f6992d8654e2e6dc6ce60aee841a82ca
sardaryandavid <74458820+sardaryandavid@users.noreply.github.com>
2022-10-17 19:20:36 +0300

    Add files via upload

--- include/Stack.h
+++ include/Stack.h
@@ -2,78 +2,71 @@
 #define STACK_H
 
 #include <cstddef>
-#include "Stack.h"
+#include <cstdint>
 #include <iostream>
 
-namespace containers 
-{
-    static const int BITS_IN_BYTE = 8;
+namespace containers {
+static const int BITS_IN_BYTE = 8;
 
-    template <typename T>
-    class Stack
-    {
-        enum {NO = 0, YES};
+template <typename T>
+class Stack {
+public:
+    Stack();
+    Stack(size_t mx_size);
+    Stack(const Stack& stack);
+    Stack(Stack&& stack);
+    ~Stack();
 
-    public:
-        Stack ();
-        Stack (size_t mx_size);
-        Stack (const Stack& stack);
-        Stack (Stack&& stack);
-        ~Stack ();
+    void push(const T& val);
+    void pop();
 
-        void push (const T& val);
-        void pop ();
-        
-        Stack& operator= (const Stack&  );
-        Stack& operator= (Stack&& );
+    Stack& operator=(const Stack&);
+    Stack& operator=(Stack&&);
 
-        int isFull  () const;
-        int isEmpty () const;
-        size_t get_size () const;
-        size_t get_mx_size () const;
-        T* get_pdata () const;
+    int isFull() const;
+    int isEmpty() const;
+    size_t get_size() const;
+    size_t get_mx_size() const;
+    T get_elem(int num) const;
 
-    private:
-        T* data_;
-        size_t mx_size_;
-        size_t size_;
-    };
+private:
+    T* data_;
+    size_t mx_size_;
+    size_t size_;
+};
 
-    template<>
-    class Stack<bool>
-    {
-        enum {NO = 0, YES};
+template <>
+class Stack<bool> {
+public:
+    Stack();
+    Stack(size_t mx_size);
+    Stack(const Stack& stack);
+    Stack(Stack&& stack);
+    ~Stack();
 
-    public:
-        Stack ();
-        Stack (size_t mx_size);
-        Stack (const Stack& stack);
-        Stack (Stack&& stack);
-        ~Stack ();
+    void push(const uint8_t& val);
+    void pop();
 
-        void push (const unsigned char& val);
-        void pop ();
-        
-        Stack& operator= (const Stack&  );
-        Stack& operator= (Stack&& );
+    Stack& operator=(const Stack&);
+    Stack& operator=(Stack&&);
 
-        int isFull  () const;
-        int isEmpty () const;
-        size_t get_size () const;
-        size_t get_mx_size () const;
-        unsigned char* get_pdata () const;
+    int isFull() const;
+    int isEmpty() const;
+    size_t get_size() const;
+    size_t get_mx_size() const;
+    uint8_t get_elem(int num) const;
 
-    private:
-        unsigned char* data_;
-        size_t mx_size_;
-        size_t size_;
-    };
+    static size_t bits_to_bytes(size_t);
 
-    size_t bits_to_bytes (size_t );
+private:
+    uint8_t* data_;
+    size_t mx_size_;
+    size_t size_;
+};
 }
 
 template <typename T>
-containers::Stack<T>::Stack ()
+containers::Stack<T>::Stack()
 {
     data_ = nullptr;
     mx_size_ = 0;
@@ -81,7 +74,7 @@ containers::Stack<T>::Stack ()
 }
 
 template <typename T>
-containers::Stack<T>::Stack (size_t mx_size)
+containers::Stack<T>::Stack(size_t mx_size)
 {
     mx_size_ = mx_size;
     size_ = 0;
@@ -89,7 +82,7 @@ containers::Stack<T>::Stack (size_t mx_size)
 }
 
 template <typename T>
-containers::Stack<T>::Stack (const Stack& stack)
+containers::Stack<T>::Stack(const Stack& stack)
 {
     data_ = new T[stack.mx_size];
     size_ = stack.size_;
@@ -99,34 +92,37 @@ containers::Stack<T>::Stack (const Stack& stack)
 }
 
 template <typename T>
-containers::Stack<T>::Stack (Stack&& stack)
+containers::Stack<T>::Stack(Stack&& stack)
 {
-    data_ = stack.data_;    
+    data_ = stack.data_;
     size_ = stack.size_;
     mx_size_ = stack.mx_size_;
     stack.data_ = nullptr;
 }
 
 template <typename T>
-containers::Stack<T>::~Stack ()
+containers::Stack<T>::~Stack()
 {
-    delete [] data_;
+    delete[] data_;
 }
 
 template <typename T>
-containers::Stack<T>& containers::Stack<T>::operator= (const Stack&  stack)
+containers::Stack<T>& containers::Stack<T>::operator=(const Stack& stack)
 {
+    if (this == &stack)
+        return *this;
+
     mx_size_ = stack.mx_size_;
     size_ = stack.size_;
 
     for (size_t i = 0; i < size_; ++i)
         data_[i] = stack.data_[i];
-    
+
     return *this;
 }
 
 template <typename T>
-containers::Stack<T>& containers::Stack<T>::operator= (Stack&& stack)
+containers::Stack<T>& containers::Stack<T>::operator=(Stack&& stack)
 {
     data_ = stack.data_;
     mx_size_ = stack.mx_size_;
@@ -138,69 +134,70 @@ containers::Stack<T>& containers::Stack<T>::operator= (Stack&& stack)
 
 /*****************************************/
 
-const int BITS_IN_BYTE = 8;
-
-size_t containers::bits_to_bytes (size_t mx_size)
-{   
+size_t containers::Stack<bool>::bits_to_bytes(size_t mx_size)
+{
     size_t bytes = mx_size / BITS_IN_BYTE;
-    
+
     if (mx_size % BITS_IN_BYTE == 0)
         return bytes;
-    
+
     else
         return bytes + 1;
 }
 
-containers::Stack<bool>::Stack ()
+containers::Stack<bool>::Stack()
 {
     data_ = nullptr;
     mx_size_ = 0;
     size_ = 0;
 }
 
-containers::Stack<bool>::Stack (size_t mx_size)
+containers::Stack<bool>::Stack(size_t mx_size)
 {
     mx_size_ = mx_size;
     size_ = 0;
 
-    size_t size_in_bytes = bits_to_bytes (mx_size);
-    data_ = new unsigned char[size_in_bytes];
+    size_t size_in_bytes = bits_to_bytes(mx_size);
+    data_ = new uint8_t[size_in_bytes];
 }
 
-containers::Stack<bool>::Stack (const Stack& stack)
+containers::Stack<bool>::Stack(const Stack& stack)
 {
-    data_ = new unsigned char[stack.mx_size_];
+    data_ = new uint8_t[stack.mx_size_];
     size_ = stack.size_;
 
     for (size_t i = 0; i < stack.size_; ++i)
         data_[i] = stack.data_[i];
 }
 
-containers::Stack<bool>::Stack (Stack&& stack)
+containers::Stack<bool>::Stack(Stack&& stack)
 {
-    data_ = stack.data_;    
+    data_ = stack.data_;
     size_ = stack.size_;
     mx_size_ = stack.mx_size_;
     stack.data_ = nullptr;
 }
 
-containers::Stack<bool>::~Stack ()
+containers::Stack<bool>::~Stack()
 {
-    delete [] data_;
+    delete[] data_;
 }
 
-containers::Stack<bool>& containers::Stack<bool>::operator= (const Stack&  stack)
+containers::Stack<bool>& containers::Stack<bool>::operator=(const Stack& stack)
 {
+    if (this == &stack)
+        return *this;
+
     mx_size_ = stack.mx_size_;
     size_ = stack.size_;
 
     for (size_t i = 0; i < size_; ++i)
         data_[i] = stack.data_[i];
-    
+
     return *this;
 }
 
-containers::Stack<bool>& containers::Stack<bool>::operator= (Stack&& stack)
+containers::Stack<bool>& containers::Stack<bool>::operator=(Stack&& stack)
 {
     data_ = stack.data_;
     mx_size_ = stack.mx_size_;
@@ -213,113 +210,108 @@ containers::Stack<bool>& containers::Stack<bool>::operator= (Stack&& stack)
 /*****************************************/
 
 template <typename T>
-size_t containers::Stack<T>::get_size () const
+size_t containers::Stack<T>::get_size() const
 {
     return this->size_;
 }
 
 template <typename T>
-size_t containers::Stack<T>::get_mx_size () const
+size_t containers::Stack<T>::get_mx_size() const
 {
     return this->mx_size_;
 }
 
-template<typename T>
-T* containers::Stack<T>::get_pdata () const
+template <typename T>
+T containers::Stack<T>::get_elem(int num) const
 {
-    return this->data_;
+    return this->data_[num];
 }
 
-template<typename T>
-int containers::Stack<T>::isFull  () const
+template <typename T>
+int containers::Stack<T>::isFull() const
 {
-    return (this->size_ == this->mx_size_) ? YES : NO;
-}
-    
-template<typename T>
-int containers::Stack<T>::isEmpty () const
-{
-    return (this->size_ == 0) ? YES : NO;
+    return (this->size_ == this->mx_size_) ? true : false;
 }
 
-size_t containers::Stack<bool>::get_size () const
+template <typename T>
+int containers::Stack<T>::isEmpty() const
+{
+    return (this->size_ == 0) ? true : false;
+}
+
+size_t containers::Stack<bool>::get_size() const
 {
     return this->size_;
 }
 
-size_t containers::Stack<bool>::get_mx_size () const
+size_t containers::Stack<bool>::get_mx_size() const
 {
     return this->mx_size_;
 }
 
-unsigned char* containers::Stack<bool>::get_pdata () const
+uint8_t containers::Stack<bool>::get_elem(int num) const
 {
-    return this->data_;
+    return this->data_[num];
 }
 
-int containers::Stack<bool>::isFull  () const
+int containers::Stack<bool>::isFull() const
 {
-    return (this->size_ == this->mx_size_) ? YES : NO;
+    return (this->size_ == this->mx_size_) ? true : false;
 }
-    
-int containers::Stack<bool>::isEmpty () const
+
+int containers::Stack<bool>::isEmpty() const
 {
-    return (this->size_ == 0) ? YES : NO;
+    return (this->size_ == 0) ? true : false;
 }
 
 /*****************************************/
 
-template<typename T>
-void containers::Stack<T>::push (const T& val)
+template <typename T>
+void containers::Stack<T>::push(const T& val)
 {
-    if (this->isFull ()) 
-    {
+    if (this->isFull()) {
         std::cout << "Stack is full" << std::endl;
-        exit (EXIT_FAILURE);
+        exit(EXIT_FAILURE);
     }
-
 
     data_[size_++] = val;
 }
 
-template<typename T>
-void containers::Stack<T>::pop ()
+template <typename T>
+void containers::Stack<T>::pop()
 {
-    if (this->isEmpty ()) 
-    {
+    if (this->isEmpty()) {
         std::cout << "Stack is empty" << std::endl;
-        exit (EXIT_FAILURE);
+        exit(EXIT_FAILURE);
     }
 
     --size_;
 }
 
-void containers::Stack<bool>::push (const unsigned char& val)
+void containers::Stack<bool>::push(const uint8_t& val)
 {
-    if (this->isFull ()) 
-    {
+    if (this->isFull()) {
         std::cout << "Stack is full" << std::endl;
-        exit (EXIT_FAILURE);
+        exit(EXIT_FAILURE);
     }
 
     size_++;
 
-    int byte_num = containers::bits_to_bytes (size_) - 1;
-    int curr_pos = (size_ -1) % containers::BITS_IN_BYTE;
+    int byte_num = containers::Stack<bool>::bits_to_bytes(size_) - 1;
+    int curr_pos = (size_ - 1) % containers::BITS_IN_BYTE;
 
     if (val == 1)
         data_[byte_num] += (1 << (containers::BITS_IN_BYTE - curr_pos - 1));
 }
 
-void containers::Stack<bool>::pop ()
+void containers::Stack<bool>::pop()
 {
-    if (this->isEmpty ()) 
-    {
+    if (this->isEmpty()) {
         std::cout << "Stack is empty" << std::endl;
-        exit (EXIT_FAILURE);
+        exit(EXIT_FAILURE);
     }
 
     --size_;
 }
 
-#endif 
+#endif
--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,212 +1,206 @@
-#include <gtest/gtest.h>
+#include "Stack.h"
 #include <gmock/gmock.h>
-#include "../include/Stack.h"
+#include <gtest/gtest.h>
 
-using namespace containers;
-
-TEST(get_size, test) 
+TEST(stackTest, get_size)
 {
     int val = 5;
     const size_t mx_size = 10;
-    Stack<int> stack {mx_size};
+    containers::Stack<int> stack { mx_size };
     stack.push(val);
 
-    ASSERT_TRUE(stack.get_size () == 1);
+    ASSERT_TRUE(stack.get_size() == 1);
 }
 
-TEST(get_mx_size, test) 
+TEST(stackTest, get_mx_size)
 {
     const size_t mx_size = 10;
-    Stack<int> stack {mx_size};
+    containers::Stack<int> stack { mx_size };
 
-    ASSERT_TRUE(stack.get_mx_size () == mx_size);
+    ASSERT_TRUE(stack.get_mx_size() == mx_size);
 }
 
-TEST(push, test) 
+TEST(stackTest, push)
 {
     int val = 5;
     const int mx_size = 10;
-    Stack<int> stack {mx_size};
-    
-    for (int i = 0; i < mx_size; ++i)
-        stack.push (val + i);
+    containers::Stack<int> stack { mx_size };
 
     for (int i = 0; i < mx_size; ++i)
-        ASSERT_TRUE(stack.get_pdata()[i] == val + i);
+        stack.push(val + i);
+
+    for (int i = 0; i < mx_size; ++i)
+        ASSERT_TRUE(stack.get_elem(i) == val + i);
 }
 
-TEST(pop, test) 
+TEST(stackTest, pop)
 {
     int val = 5;
     const size_t mx_size = 10;
-    Stack<int> stack {mx_size};
-    stack.push (val);
-    stack.pop ();
+    containers::Stack<int> stack { mx_size };
+    stack.push(val);
+    stack.pop();
 
-    ASSERT_TRUE(stack.get_size () == 0);
+    ASSERT_TRUE(stack.get_size() == 0);
 }
 
-TEST(isFull, test) 
+TEST(stackTest, isFull)
 {
     int val = 5;
     const size_t mx_size = 10;
-    Stack<int> stack {mx_size};
-    
-    for (size_t i = 0; i < mx_size; ++i)
-        stack.push (val + i);
-    
-    ASSERT_TRUE(stack.isFull () == 1);
-}
-
-TEST(isEmpty, test) 
-{
-    const size_t mx_size = 10;
-    Stack<int> stack {mx_size};
-    
-    ASSERT_TRUE(stack.isEmpty () == 1);
-}
-
-TEST(operator_assign, test)
-{
-    int val = 5;
-    const size_t mx_size = 10;
-    Stack<int> stack1 {mx_size};
-    Stack<int> stack2 {mx_size};
+    containers::Stack<int> stack { mx_size };
 
     for (size_t i = 0; i < mx_size; ++i)
-        stack1.push (val + i);    
-    
+        stack.push(val + i);
+
+    ASSERT_TRUE(stack.isFull() == 1);
+}
+
+TEST(stackTest, isEmpty)
+{
+    const size_t mx_size = 10;
+    containers::Stack<int> stack { mx_size };
+
+    ASSERT_TRUE(stack.isEmpty() == 1);
+}
+
+TEST(stackTest, operator_assign)
+{
+    int val = 5;
+    const size_t mx_size = 10;
+    containers::Stack<int> stack1 { mx_size };
+    containers::Stack<int> stack2 { mx_size };
+
+    for (size_t i = 0; i < mx_size; ++i)
+        stack1.push(val + i);
+
     stack2 = stack1;
 
-    ASSERT_TRUE(stack1.get_mx_size () == stack2.get_mx_size () &&
-                stack1.get_size    () == stack2.get_size    ());
-    
+    ASSERT_TRUE(stack1.get_mx_size() == stack2.get_mx_size() && stack1.get_size() == stack2.get_size());
+
     for (size_t i = 0; i < mx_size; ++i)
-        ASSERT_TRUE(stack1.get_pdata()[i] == stack1.get_pdata()[i]);
+        ASSERT_TRUE(stack1.get_elem(i) == stack1.get_elem(i));
 }
 
-TEST(move_operator_assign, test) 
+TEST(stackTest, move_operator_assign)
 {
     int val = 5;
     const size_t mx_size = 10;
-    Stack<int> stack2 {mx_size};
+    containers::Stack<int> stack2 { mx_size };
 
     for (size_t i = 0; i < mx_size; ++i)
-        stack2.push (val + i);    
-    
-    Stack<int> stack1 = std::move(stack2);
+        stack2.push(val + i);
 
-    ASSERT_TRUE(stack1.get_mx_size () == stack2.get_mx_size () &&
-                stack1.get_size    () == stack2.get_size    ());
-    
+    containers::Stack<int> stack1 = std::move(stack2);
+
+    ASSERT_TRUE(stack1.get_mx_size() == stack2.get_mx_size() && stack1.get_size() == stack2.get_size());
+
     for (size_t i = 0; i < mx_size; ++i)
-        ASSERT_TRUE(stack1.get_pdata()[i] == stack1.get_pdata()[i]);
+        ASSERT_TRUE(stack1.get_elem(i) == stack1.get_elem(i));
 }
 
 /**************************************/
 
-TEST(get_size_bool, test) 
+TEST(boolStackTest, get_size_bool)
 {
     int val = 5;
     const size_t mx_size = 10;
-    Stack<bool> stack {mx_size};
+    containers::Stack<bool> stack { mx_size };
     stack.push(val);
 
-    ASSERT_TRUE(stack.get_size () == 1);
+    ASSERT_TRUE(stack.get_size() == 1);
 }
 
-TEST(get_mx_size_bool, test) 
+TEST(boolStackTest, get_mx_size_bool)
 {
     const size_t mx_size = 10;
-    Stack<bool> stack {mx_size};
+    containers::Stack<bool> stack { mx_size };
 
-    ASSERT_TRUE(stack.get_mx_size () == mx_size);
+    ASSERT_TRUE(stack.get_mx_size() == mx_size);
 }
 
-TEST(push_bool, test) 
+TEST(boolStackTest, push_bool)
 {
     int val = 1;
     const int mx_size = 10;
-    Stack<bool> stack {mx_size};
-    
-    for (int i = 0; i < mx_size; ++i)
-        stack.push (val);
+    containers::Stack<bool> stack { mx_size };
 
-    ASSERT_TRUE(stack.get_pdata()[0] == 255);
-    ASSERT_TRUE(stack.get_pdata()[1] == 192);
+    for (int i = 0; i < mx_size; ++i)
+        stack.push(val);
+
+    ASSERT_TRUE(stack.get_elem(0) == 255);
+    ASSERT_TRUE(stack.get_elem(1) == 192);
 }
 
-TEST(pop_bool, test) 
+TEST(boolStackTest, pop_bool)
 {
     int val = 5;
     const size_t mx_size = 10;
-    Stack<bool> stack {mx_size};
-    stack.push (val);
-    stack.pop ();
+    containers::Stack<bool> stack { mx_size };
+    stack.push(val);
+    stack.pop();
 
-    ASSERT_TRUE(stack.get_size () == 0);
+    ASSERT_TRUE(stack.get_size() == 0);
 }
 
-TEST(isFull_bool, test) 
+TEST(boolStackTest, isFull_bool)
 {
     int val = 1;
     const size_t mx_size = 10;
-    Stack<bool> stack {mx_size};
-    
+    containers::Stack<bool> stack { mx_size };
+
     for (size_t i = 0; i < mx_size; ++i)
-        stack.push (val);
-    
-    ASSERT_TRUE(stack.isFull () == 1);
+        stack.push(val);
+
+    ASSERT_TRUE(stack.isFull() == 1);
 }
 
-TEST(isEmpty_bool, test) 
+TEST(boolStackTest, isEmpty_bool)
 {
     const size_t mx_size = 10;
-    Stack<bool> stack {mx_size};
-    
-    ASSERT_TRUE(stack.isEmpty () == 1);
+    containers::Stack<bool> stack { mx_size };
+
+    ASSERT_TRUE(stack.isEmpty() == 1);
 }
 
-TEST(operator_assign_bool, test)
+TEST(boolStackTest, operator_assign_bool)
 {
     int val = 1;
     const size_t mx_size = 10;
-    Stack<bool> stack1 {mx_size};
-    Stack<bool> stack2 {mx_size};
+    containers::Stack<bool> stack1 { mx_size };
+    containers::Stack<bool> stack2 { mx_size };
 
     for (size_t i = 0; i < mx_size; ++i)
-        stack1.push (val);    
-    
+        stack1.push(val);
+
     stack2 = stack1;
 
-    ASSERT_TRUE(stack1.get_mx_size () == stack2.get_mx_size () &&
-                stack1.get_size    () == stack2.get_size    ());
-    
-    for (size_t i = 0; i < containers::bits_to_bytes (mx_size); ++i)
-        ASSERT_TRUE(stack1.get_pdata()[i] == stack1.get_pdata()[i]);
+    ASSERT_TRUE(stack1.get_mx_size() == stack2.get_mx_size() && stack1.get_size() == stack2.get_size());
+
+    for (size_t i = 0; i < containers::Stack<bool>::bits_to_bytes(mx_size); ++i)
+        ASSERT_TRUE(stack1.get_elem(i) == stack2.get_elem(i));
 }
 
-TEST(move_operator_assign_bool, test) 
+TEST(boolStackTest, move_operator_assign_bool)
 {
     int val = 1;
     const size_t mx_size = 10;
-    Stack<bool> stack2 {mx_size};
+    containers::Stack<bool> stack2 { mx_size };
 
     for (size_t i = 0; i < mx_size; ++i)
-        stack2.push (val);    
-    
-    Stack<bool> stack1 = std::move(stack2);
+        stack2.push(val);
 
-    ASSERT_TRUE(stack1.get_mx_size () == stack2.get_mx_size () &&
-                stack1.get_size    () == stack2.get_size    ());
-    
-    for (size_t i = 0; i < containers::bits_to_bytes (mx_size); ++i)
-        ASSERT_TRUE(stack1.get_pdata()[i] == stack1.get_pdata()[i]);
+    containers::Stack<bool> stack1 = std::move(stack2);
+
+    ASSERT_TRUE(stack1.get_mx_size() == stack2.get_mx_size() && stack1.get_size() == stack2.get_size());
+
+    for (size_t i = 0; i < containers::Stack<bool>::bits_to_bytes(mx_size); ++i)
+        ASSERT_TRUE(stack1.get_elem(i) == stack1.get_elem(i));
 }
 
 int main(int argc, char** argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
-  
+
     return RUN_ALL_TESTS();
 }
